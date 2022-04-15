cfbfeb88907e60341a63a19a446b108dd6fb1af0
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-15 10:51:59 +0200

    Initial Commit

---
+++ GridCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridCalendar
+//
+//  Created by Esteban Calvete Iglesias on 15/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Esteban Calvete Iglesias on 15/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
